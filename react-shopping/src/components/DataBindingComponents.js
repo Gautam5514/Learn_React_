b8a55bbc76f1e19ad9ae6d0c708e8e92b177390e
Gautam pandit <gautampandit797@gmail.com>
2024-09-11 00:12:59 +0530

    help to make more reliable and useful

--- react-shopping/src/components/DataBindingComponents.js
+++ react-shopping/src/components/DataBindingComponents.js
@@ -1,21 +1,50 @@
 
 
 export default function DataBindingComponents() {
-    var product = {
-        Name: 'Samsung TV',
-        Price: 45005.54,
-        stock: true
-    }
+    var catagories = ["All", "Electronics", "Footwear", "Fashion"];
     return(
         <div className="container">
-            <h2>Product Details</h2>
-            <dl>
-                <dt>Name</dt>
-                <dd>{product.Name}</dd>
-                <dd><input type="text" value={product.Name} /></dd>
-                <dt>price</dt>
-                <dd>{product.Price}</dd>
-            </dl>
+            <h2>Categories</h2>
+            <ol>
+                {
+                    catagories.map(catogary => 
+                        <li key={catogary}>{catogary}</li>
+                    )
+                    
+                }
+            </ol>
+            <h2>Select Item</h2>
+            <select>
+                {
+                    catagories.map(category => 
+                        <option key={category} value={category}>{category}</option>
+                    )
+                }
+            </select>
+
         </div>
     )
 }
+
+
+
+
+// var product = {
+//     Name: 'Samsung TV',
+//     Price: 45005.54,
+//     stock: true
+// }
+// return(
+//     <div className="container">
+//         <h2>Product Details</h2>
+//         <dl>
+//             <dt>Name</dt>
+//             <dd>{product.Name}</dd>
+//             <dd><input type="text" value={product.Name} /></dd>
+//             <dt>price</dt>
+//             <dd>{product.Price}</dd>
+//             <dt>Stocks</dt>
+//             <dd>{(product.stock == true) ? "Available" : "Out of"}</dd>
+//         </dl>
+//     </div>
+// )
